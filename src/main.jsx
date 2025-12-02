e64264eb5f243db43e857d09b5df8f071afedf20
SD Sharma <sdsharma.03082003@gmail.com>
2025-12-02 12:30:25 +0530

    Browser router for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"; 
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <App />
+     <BrowserRouter basename="/UI_Design_React_Project"> {/* Replace /myApp with your repo name */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
+  
+ /* <BrowserRouter basename="/myApp">
+    <App />
+  </BrowserRouter>*/
 )
